953b60f2c75fb63ee139605570e58827ac670ad2
MasterGammi <54286127+MasterGammi@users.noreply.github.com>
2026-01-10 23:29:21 +0700

    Update JoystickHandler.cs

--- UIScripts/Joystick/JoystickHandler.cs
+++ UIScripts/Joystick/JoystickHandler.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class JoystickHandler : MonoBehaviour
+public abstract class JoystickHandler : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Image _joystickBackgraund;
+    [SerializeField] private Image _joystick;
+    [SerializeField] private Image _joystickArea;
+
+    private Vector2 _joystickPosition;
+
+    protected Vector2 _joystickVector;
+
+    [SerializeField] private Color[] _joystickColor = new Color[2];
+
+    private void Start()
     {
-        
+        _joystickPosition = _joystickBackgraund.rectTransform.anchoredPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
     }
 }
